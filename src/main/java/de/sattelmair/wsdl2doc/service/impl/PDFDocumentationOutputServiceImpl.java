53d5681c27231d069d04db8f7295965f283680a0
erik <erik.sattelmair@gmx.de>
2019-05-27 20:38:09 +0200

    PDF documentation generator added

--- src/main/java/de/sattelmair/wsdl2doc/service/impl/PDFDocumentationOutputServiceImpl.java
+++ src/main/java/de/sattelmair/wsdl2doc/service/impl/PDFDocumentationOutputServiceImpl.java
@@ -1,13 +1,38 @@
 package de.sattelmair.wsdl2doc.service.impl;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
 import de.sattelmair.wsdl2doc.service.DocumentationOutputService;
+import lombok.extern.slf4j.Slf4j;
 import org.ow2.easywsdl.wsdl.api.Description;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@Slf4j
 public class PDFDocumentationOutputServiceImpl implements DocumentationOutputService {
 
     @Override
     public byte[] generateDocumentation(Description serviceDescription) {
-        return new byte[0];
+        final ByteArrayOutputStream result = new ByteArrayOutputStream();
+
+        final Document document = new Document();
+        try {
+            final PdfWriter writer = PdfWriter.getInstance(document, result);
+            document.open();
+
+            final DocumentationOutputService documentationOutputService = new HTMLDocumentationOutputServiceImpl();
+            XMLWorkerHelper.getInstance().parseXHtml(writer, document,
+                    new ByteArrayInputStream(documentationOutputService.generateDocumentation(serviceDescription)));
+            document.close();
+        } catch (DocumentException | IOException e) {
+            log.error("ERROR while trying to create webservice documentation from WSDL!!");
+        }
+
+        return result.toByteArray();
     }
 
 }
--- src/test/java/de/sattelmair/wsdl2doc/WSDLImportTest.java
+++ src/test/java/de/sattelmair/wsdl2doc/WSDLImportTest.java
@@ -2,9 +2,7 @@ package de.sattelmair.wsdl2doc;
 
 import de.sattelmair.wsdl2doc.service.DocumentationOutputService;
 import de.sattelmair.wsdl2doc.service.WSDLImportService;
-import de.sattelmair.wsdl2doc.service.impl.HTMLDocumentationOutputServiceImpl;
-import de.sattelmair.wsdl2doc.service.impl.MarkdownDocumentationOutputServiceImpl;
-import de.sattelmair.wsdl2doc.service.impl.WSDLImportServiceImpl;
+import de.sattelmair.wsdl2doc.service.impl.*;
 import org.junit.Test;
 import org.ow2.easywsdl.wsdl.api.Description;
 import org.ow2.easywsdl.wsdl.api.WSDLImportException;
@@ -34,6 +32,26 @@ public class WSDLImportTest {
 
         final DocumentationOutputService documentationOutputService = new HTMLDocumentationOutputServiceImpl();
         File file = new File("src/test/resources/de/sattelmair/wsdl2doc/output/output_html.html");
+        Files.write(file.toPath(), documentationOutputService.generateDocumentation(description));
+    }
+
+    @Test
+    public void pdf_documentation_creator_test() throws WSDLImportException, IOException {
+        final WSDLImportService wsdlImportService = new WSDLImportServiceImpl();
+        final Description description = wsdlImportService.readWSDLFromFile(WSDL_FILE_PATH);
+
+        final DocumentationOutputService documentationOutputService = new PDFDocumentationOutputServiceImpl();
+        File file = new File("src/test/resources/de/sattelmair/wsdl2doc/output/output_pdf.pdf");
+        Files.write(file.toPath(), documentationOutputService.generateDocumentation(description));
+    }
+
+    @Test
+    public void word_documentation_creator_test() throws WSDLImportException, IOException {
+        final WSDLImportService wsdlImportService = new WSDLImportServiceImpl();
+        final Description description = wsdlImportService.readWSDLFromFile(WSDL_FILE_PATH);
+
+        final DocumentationOutputService documentationOutputService = new WordDocumentationOutputServiceImpl();
+        File file = new File("src/test/resources/de/sattelmair/wsdl2doc/output/output_word.docx");
         Files.write(file.toPath(), documentationOutputService.generateDocumentation(description));
     }
 }
